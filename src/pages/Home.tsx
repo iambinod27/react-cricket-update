c26a96e6e9bb6053d902aad535ab6eeb8e82e685
binod waiba tamang <helloitsbinod@gmail.com>
2023-12-19 22:56:05 +0545

    feat: typematches mapped

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,49 @@
 import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
 import { getMatches } from "../store/action/matches/matchesActions";
 import { useEffect } from "react";
+import { RootState } from "@/store/store";
 
 const Home = () => {
   const dispatch = useAppDispatch();
-  const { matchesList } = useAppSelector((state) => state.matches);
+  const { matchesList, isLoading } = useAppSelector(
+    (state: RootState) => state.matches
+  );
 
-  console.log(matchesList);
+  console.log(matchesList.typeMatches);
+
+  const matches: Array[] = matchesList.typeMatches;
+
+  console.log(matches);
 
   useEffect(() => {
     dispatch(getMatches());
   }, [dispatch]);
 
-  return <div>Home</div>;
+  return (
+    <>
+      {isLoading ? (
+        <>
+          <p>Loading...</p>
+        </>
+      ) : (
+        <div>
+          {matches.map((match: string) => {
+            const seriesMatches = match.seriesMatches;
+            console.log(seriesMatches);
+            return (
+              <>
+                <h3>{match.matchType}</h3>
+                {/* <div>
+                  {seriesMatches.map((series) => {
+                    return <p>{series.seriesAdWrapper.seriesName}</p>;
+                  })}
+                </div> */}
+              </>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
 };
 export default Home;
--- src/store/features/matches/matchesSlice.tsx
+++ src/store/features/matches/matchesSlice.tsx
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { getMatches } from "../../action/matches/matchesActions.tsx";
 
 const initialState = {
-  isLoading: false,
+  isLoading: true,
   matchesList: [],
 };
 
